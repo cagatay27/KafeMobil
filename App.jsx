8406718a9c517a24cdb9ba67e0e4c969c3dc22bf
cagatay27 <120118172+cagatay27@users.noreply.github.com>
2025-04-07 15:00:49 +0300

    Kullanıcı arayüz geliştirildi

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import {NavigationContainer} from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+import {CartProvider} from './src/context/CartContext';
+
+const App = () => {
+  return (
+    <CartProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </CartProvider>
+  );
+};
+
+export default App;
